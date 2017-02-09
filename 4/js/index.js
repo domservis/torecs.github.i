8a480b3a656e8aa747ac7d2a6f01bf859bad706d
torecs <Mr-redi@mail.ru>
2017-02-09 15:12:47 +0300

    Update index.js

--- 4/js/index.js
+++ 4/js/index.js
@@ -53,25 +53,3 @@ var init = function() {
 };
 
 doc.on('ready', init);
-
-
-window.ChatraSetup = {
-    mode: 'frame',
-    injectTo: 'chatra-wrapper' 
-};
-    ChatraID = 'tBXJTuu2psNABNcC2';
-    (function(d, w, c) {
-        var n = d.getElementsByTagName('script')[0],
-            s = d.createElement('script');
-        w[c] = w[c] || function() {
-            (w[c].q = w[c].q || []).push(arguments);
-        };
-        s.async = true;
-        s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
-            + '//call.chatra.io/chatra.js';
-        n.parentNode.insertBefore(s, n);
-    })(document, window, 'Chatra');
-	
-	   Chatra('setColors', {
-    chatBg: 'transparent' // фон
-});
